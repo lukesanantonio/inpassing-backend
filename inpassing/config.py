7a408eb5186ef5a7adce026f0629ee1592cd6077
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2016-11-23 14:06:36 -0500

    Use a secret key (get it from an env var)

--- inpassing/config.py
+++ inpassing/config.py
@@ -1,5 +1,8 @@
 # Copyright (c) 2016 Luke San Antonio Bialecki
 # All rights reserved.
 
+import os
+SECRET_KEY=os.getenv('INPASSING_SECRET_KEY')
+
 SQLALCHEMY_DATABASE_URI='sqlite:///db.sqlite3'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
