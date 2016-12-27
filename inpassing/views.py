86b9dfd0ad957a3c8eb71d5d432dd111b13e0e7c
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2016-12-26 19:45:50 -0500

    Add endpoints for managing moderators and participants of an org
    They are all mostly untested.

--- inpassing/views.py
+++ inpassing/views.py
@@ -95,6 +95,218 @@ def orgs_search():
     orgs = db.session.query(Org).filter(Org.name.like('%' + query + '%')).all()
     return jsonify([{'id': org.id, 'name': org.name } for org in orgs]), 200
 
+# Org participants
+@app.route('/orgs/<org_id>/participants', methods=['GET', 'POST'])
+def org_participants(org_id):
+
+    org = Org.query.filter_by(id=org_id).first()
+    if org == None:
+        return jsonify({
+            'msg': 'org not found'
+        }), 404
+
+    client_user_id = get_jwt_identity()
+
+    if user_is_mod(client_user_id, org_id):
+        # If the user is a mod we are going to make whoever they want
+        # participate, guaranteed.
+        mod_request = True
+    elif user_is_participant(client_user_id, org_id):
+        # If the user is a participant we take their user as a suggestion
+        mod_request = False
+    else:
+        # Otherwise fuck'em
+        return jsonify({
+            'msg': 'user {} must moderate or participate in org {}'.format(
+                client_user_id, org_id
+            )
+        }), 403
+
+    if request.method == 'POST':
+        js = request.get_json()
+
+        # Make sure we were given a valid id
+        participant_id = js.get('user_id')
+        if participant_id == None:
+            return jsonify({
+                'msg': 'missing user_id'
+            }), 422
+
+        # And that the ID represents a valid user
+        participant = User.query.filter_by(id=participant_id).first()
+        if participant == None:
+            return jsonify({
+                'msg': 'unknown user {}'.format(participant_id)
+            }), 422
+
+        # Only allow regular participants to add themselves.
+        if not mod_request and participant_id != client_user_id:
+            return jsonify({
+                'msg': 'participant {} should only be adding itself'
+            }), 422
+
+        # Make sure that the participant to be is not already a participant
+        if participant in org.participants:
+            return jsonify({
+                'msg': 'user {} already participates in org {}'.format(
+                    participant_id, org_id
+                )
+            }), 204
+
+        # Do it now!
+        org.participants.append(participant)
+        db.session.commit()
+
+        return '', 204
+
+    else:
+        # The method is GET
+        if mod_request:
+            # Return all participants
+            return jsonify([
+                util.user_dict(user) for user in org.participants
+            ]), 200
+        else:
+            # They know what they did wrong.
+            return '', 403
+
+@app.route('/orgs/<org_id>/participants/<user_id>')
+@jwt_required
+def org_participants_query(org_id, user_id):
+    # Find the org in question
+    org = Org.query.filter_by(id=org_id).first()
+    if org == None:
+        return jsonify({
+            'msg': 'org not found'
+        }), 404
+
+    # Is the user a participant?
+    user = User.query.filter_by(user_id).first()
+
+    if user in org.participants:
+        participating = True
+    else:
+        participating = False
+
+    user_ret = jsonify(util.user_dict(user)), 200
+    no_part_ret = jsonify({
+        'msg': 'user {} does not participate in org {}'.format(user_id, org_id)
+    }), 404
+
+    # If the client is a mod, let them see this unrestricted.
+    client_user_id = get_jwt_identity()
+
+    # This logic could be less redundant but I think its clear what is going on
+    # its current form. Basically, if the user is a participant they can only
+    # really query their own object as an easy check to see if a user
+    # participates in a given org. For mods though they can query any user
+    # object, or the entire list with the endpoint above.
+    if user_is_participant(client_user_id, org_id):
+        if client_user_id == user_id and participating:
+            # The user is asking if they are participating in this org, and they
+            # are.
+            return user_ret
+        else:
+            # No matter who it is and if they're participating, we won't tell
+            # this random client.
+            return no_part_ret
+    elif user_is_mod(client_user_id, org_id):
+        # The user is a mod, they can query whatever they want.
+        if participating:
+            return user_ret
+        else:
+            return no_part_ret
+    else:
+        return jsonify({
+            'msg': 'user {} must moderate or participate in org {}'.format(
+                client_user_id, org_id
+            )
+        }), 403
+
+# Org moderators
+@app.route('/orgs/<org_id>/moderators', methods=['GET', 'POST'])
+@jwt_required
+def org_moderators(org_id):
+
+    org = Org.query.filter_by(id=org_id).first()
+    if org == None:
+        return jsonify({
+            'msg': 'org not found'
+        }), 404
+
+    client_user_id = get_jwt_identity()
+    if request.method == 'POST':
+        # The client must be a mod
+        if not user_is_mod(client_user_id, org_id):
+            return jsonify({
+                'msg': 'user {} must moderate org {}'.format(
+                    client_user_id, org_id
+                )
+            }), 403
+
+        js = request.get_json()
+
+        # Make sure we were given a valid id
+        mod_id = js.get('user_id')
+        if mod_id == None:
+            return jsonify({
+                'msg': 'missing user_id'
+            }), 422
+
+        # And that the ID represents a valid user
+        mod = User.query.filter_by(id=mod_id).first()
+        if mod == None:
+            return jsonify({
+                'msg': 'unknown user {}'.format(mod_id)
+            }), 422
+
+        # Make sure that the participant to be is not already a participant
+        if mod in org.mods:
+            return jsonify({
+                'msg': 'user {} already moderates org {}'.format(
+                    mod_id, org_id
+                )
+            }), 204
+
+        # Do it now!
+        org.mods.append(mod)
+        db.session.commit()
+
+        return '', 204
+
+    else:
+        # The method is GET so return all moderators
+        return jsonify([
+            util.user_dict(user) for user in org.moderators
+        ]), 200
+
+@app.route('/orgs/<org_id>/moderators/<user_id>')
+def org_moderators_query(org_id, user_id):
+    # Find the org in question
+    org = Org.query.filter_by(id=org_id).first()
+    if org == None:
+        return jsonify({
+            'msg': 'org not found'
+        }), 404
+
+    # Does our client moderate the org?
+    client_user_id = get_jwt_identity()
+    if not user_is_mod(client_user_id, org_id):
+        # Get outta here
+        return jsonify({
+            'msg': 'user {} must moderate org {}'.format(client_user_id, org_id)
+        }), 403
+
+    # Does the given user moderate?
+    user = User.query.filter_by(user_id).first()
+
+    if user in org.mods:
+        return jsonify(util.user_dict(user)), 200
+    else:
+        return jsonify({
+            'msg': 'user {} does not moderate org {}'.format(user_id, org_id)
+        }), 404
+
 # Day states
 
 @app.route('/orgs/<org_id>/daystates', methods=['GET', 'POST'])
