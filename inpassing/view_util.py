0f125bb294bdbc20b56bcb42116aaca244c3c4df
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2017-01-11 20:42:14 -0500

    Add helper functions to view_util for better error handling
    One function gets a field from the request's json object while the
    other two objects query orgs and users, handling the worst case of an
    invalid id with an exception (which should generate the correct error
    response object).

--- inpassing/view_util.py
+++ inpassing/view_util.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2016 Luke San Antonio Bialecki
 # All rights reserved.
 
+from . import exceptions as ex
 from . import models
-from .models import db
+from .models import db, User, Org
 
 
 def user_is_participant(user_id, org_id):
@@ -17,3 +18,24 @@ def user_is_mod(user_id, org_id):
     q = db.session.query(models.org_mods).filter_by(mod=user_id, org=org_id)
     (ret,) = db.session.query(q.exists()).first()
     return ret
+
+
+def get_field(request, field):
+    val = request.get_json().get(field, None)
+    if val is None:
+        raise ex.MissingFieldError(field)
+    return val
+
+
+def get_org_by_id(org_id):
+    org = Org.query.filter_by(id=org_id).first()
+    if org is None:
+        raise ex.OrgNotFound(org_id)
+    return org
+
+
+def get_user_by_id(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        raise ex.UserNotFound(user_id)
+    return user
