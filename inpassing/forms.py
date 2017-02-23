9c32d62b185958cf4b8c1554e84c8607b69cc8e7
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2017-02-23 02:08:00 -0500

    Add an ugly but very functional signup page

--- inpassing/forms.py
+++ inpassing/forms.py
@@ -8,3 +8,14 @@ from wtforms import StringField, PasswordField, validators
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[validators.email()])
     password = PasswordField('Password', validators=[validators.length(6)])
+
+
+class SignupForm(FlaskForm):
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    email = StringField('Email', validators=[validators.email()])
+    password = PasswordField('Password', validators=[
+        validators.InputRequired(),
+        validators.EqualTo('confirm', 'Passwords must match')
+    ])
+    confirm = PasswordField('Confirm Password')
--- inpassing/views/admin.py
+++ inpassing/views/admin.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2016 Luke San Antonio Bialecki
 # All rights reserved.
 
-from flask import Blueprint, render_template, request, redirect
-from ..models import User
-from ..forms import LoginForm
+import bcrypt
+from flask import Blueprint, render_template, request, redirect, url_for
+from ..models import db, User
+from ..forms import LoginForm, SignupForm
 
 admin_www = Blueprint('admin', __name__, template_folder='templates_admin')
 
@@ -12,9 +13,47 @@ def index():
     return render_template('index.html')
 
 
+@admin_www.route('/home', methods=['GET'])
+def user_home():
+    return render_template('home.html')
+
+
 @admin_www.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         return redirect('/')
     return render_template('login.html', form=form)
+
+
+@admin_www.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        # TODO: Use a captcha
+
+        # Make sure a user with that email doesn't exist already
+        email = form.email.data
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user is not None:
+            # R-r-r-rip
+            return render_template('signup.html', form=form,
+                                   errors=['User already exists'])
+
+        # Make a new user
+        new_user = User(first_name=form.first_name.data,
+                        last_name=form.last_name.data,
+                        email=email)
+        new_user.password = bcrypt.hashpw(form.password.data.encode('ascii'),
+                                          bcrypt.gensalt(12))
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        # TODO: Log in the user
+        return redirect(url_for('.user_home'))
+
+    errors = []
+    for field, errs in form.errors.items():
+        errors.extend(errs)
+    return render_template('signup.html', form=form, errors=errors)
