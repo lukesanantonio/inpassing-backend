5948644c3f6bf2f69b70d82269de896ef5295b54
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2017-03-02 09:54:14 -0500

    worker/daystate: Document current_state

--- inpassing/worker/daystate.py
+++ inpassing/worker/daystate.py
@@ -9,7 +9,16 @@
 # Figure out the state based on what the last fixed index was and the number of
 # periods since it was fixed.
 
+
 def current_state(states, fixed_index, periods_since_fixed):
+    """Return the state of some day based on how it was last fixed.
+
+    Arguments:
+        states - An array of states, or anything really.
+        fixed_index - The index of the state last specified.
+        periods_since_fixed - The number of periods since the last state was
+        specified.
+    """
     # Given a list of states, the fixed state and the distance since it was
     # fixed, find the current state. This is done by simply rotating
     return states[(fixed_index + periods_since_fixed) % len(states)]
